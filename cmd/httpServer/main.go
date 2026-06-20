4e5b072854b75c2c10c38bfb4ec18d66714446b6
alex-njaiya <ngangaalex795@gmail.com>
2026-06-20 21:44:20 +0300

    update the content-type to text/html

--- cmd/httpServer/main.go
+++ cmd/httpServer/main.go
@@ -41,6 +41,7 @@ func main() {
 
 		default:
 			headers.Set("Content-Length", strconv.Itoa(len(body)))
+			headers.Set("Content-Type", "text/html")
 			status = response.StatusCode(200)
 			w.WriteStatusLine(status)
 			w.WriteHeaders(headers)
@@ -66,42 +67,42 @@ func main() {
 
 func response200() []byte {
 	return []byte(`
-	<html>
-		<head>
-			<title>200 OK</title>
-		</head>
-		<body>
-			<h1>Success!</h1>
-			<p>Your request was an absolute banger.</p>
-		</body>
-	</html>
+<html>
+	<head>
+		<title>200 OK</title>
+	</head>
+	<body>
+		<h1>Success!</h1>
+		<p>Your request was an absolute banger.</p>
+	</body>
+</html>
 	`)
 }
 
 func response400() []byte {
 	return []byte(`
-	<html>
-		<head>
-			<title>400 Bad Request</title>
-		</head>
-		<body>
-			<h1>Bad Request</h1>
-			<p>Your request honestly kinda sucked.</p>
-		</body>
-	</html>
+<html>
+	<head>
+		<title>400 Bad Request</title>
+	</head>
+	<body>
+		<h1>Bad Request</h1>
+		<p>Your request honestly kinda sucked.</p>
+	</body>
+</html>
 	`)
 }
 
 func response500() []byte {
 	return []byte(`
-	<html>
-		<head>
-			<title>500 Internal Server Error</title>
-		</head>
-		<body>
-			<h1>Internal Server Error</h1>
-			<p>Okay, you know what? This one is on me.</p>
-		</body>
-	</html>
+<html>
+	<head>
+		<title>500 Internal Server Error</title>
+	</head>
+	<body>
+		<h1>Internal Server Error</h1>
+		<p>Okay, you know what? This one is on me.</p>
+	</body>
+</html>
 	`)
 }
